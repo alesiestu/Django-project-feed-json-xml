66eb0959765b3e6d14a2b20f666c8a12dd9219c0
alesiestu <iannacone.alessandro@gmail.com>
2018-05-07 20:13:57 +0200

    Inizializzazione modulo conti e impostata view

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,3 @@
-    # encoding: utf-8
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 from jet.dashboard import modules
@@ -6,47 +5,49 @@ from jet.dashboard.dashboard import Dashboard, AppIndexDashboard
 from jet.utils import get_admin_site_name
 
 
+
+
 class CustomIndexDashboard(Dashboard):
     columns = 3
-    def init_with_context(self, context):
-        self.children.append(modules.LinkList(
-            _('Supporto'),
-            children=[
-                {
-                    'title': _('Django documentation'),
-                    'url': 'http://docs.djangoproject.com/',
-                    'external': True,
-                },
-                {
-                    'title': _('Django "django-users" mailing list'),
-                    'url': 'http://groups.google.com/group/django-users',
-                    'external': True,
-                },
-                {
-                    'title': _('Django irc channel'),
-                    'url': 'irc://irc.freenode.net/django',
-                    'external': True,
-                },
-            ],
-            column=2,
-            order=1
-        ))
 
-
-class CustomAppIndexDashboard(AppIndexDashboard):
     def init_with_context(self, context):
         self.available_children.append(modules.LinkList)
+        self.available_children.append(modules.Feed)
 
-        self.children.append(modules.ModelList(
-            title=_('Application models'),
-            models=self.models(),
+       
+
+        site_name = get_admin_site_name(context)
+        # append a link list module for "quick links"
+        self.children.append(modules.LinkList(
+            _('Quick links'),
+            layout='inline',
+            draggable=False,
+            deletable=False,
+            collapsible=False,
+            children=[
+                [_('Return to site'), '/'],
+                [_('Change password'),
+                 reverse('%s:password_change' % site_name)],
+                [_('Log out'), reverse('%s:logout' % site_name)],
+            ],
             column=0,
             order=0
         ))
-        self.children.append(modules.RecentActions(
-            include_list=self.get_app_content_types(),
+
+        # append an app list module for "Applications"
+        self.children.append(modules.AppList(
+            _('Applications'),
+            exclude=('auth.*',),
             column=1,
             order=0
-))
+        ))
 
+        # append an app list module for "Administration"
+        self.children.append(modules.AppList(
+            _('Administration'),
+            models=('auth.*',),
+            column=2,
+            order=0
+        ))
 
+       
